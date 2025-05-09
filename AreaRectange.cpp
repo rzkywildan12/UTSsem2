200e03255fbfd3c1d478ac6e7e50d6fb8d644aeb
rizkywildan <w1ld4n.r.w1j4y4@gmail.com>
2025-05-09 10:12:11 +0700

    Create AreaRectange.cpp

---
+++ AreaRectange.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length = 5;
+    int width = 3;
+
+    int area = length * width;
+
+    cout << "Area of rectangle: " << area << endl;
+
+    return 0;
+}
